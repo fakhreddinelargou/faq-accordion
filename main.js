3a4a8836266dc5b5511f33263016d933e470cf2a
fakhreddine <largofakhrddin@gmail.com>
2025-01-23 23:49:49 +0100

    modified code js

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-const accordion = document.querySelectorAll('.accordions')
+ const accordion = document.querySelectorAll('.accordions')
 
 
-accordion.forEach((acco) => {
-    acco.addEventListener('click', function () {
-        accordion.forEach((remove) => {
-            remove.classList.remove('active')
+// accordion.forEach((acco) => {
+//     acco.addEventListener('click', function () {
+//         accordion.forEach((remove) => {
+//             remove.classList.remove('active')
 
-        })
-        acco.classList.add('active')
+//         })
+//         acco.classList.add('active')
 
+//     })
+// })
+
+for(let i = 0 ; i < accordion.length ; i++){
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active')
     })
-})
-
-
-
+}
 
 
 
